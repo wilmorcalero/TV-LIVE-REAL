c011f0c09efd9700528aa29699c20a678ffff26b
wilmorcalero <72993640+wilmorcalero@users.noreply.github.com>
2025-01-13 19:55:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -65,4 +65,3 @@ function formatDateTime(dateTime) {
   const date = new Date(dateTime.slice(0, 4) + '-' + dateTime.slice(4, 6) + '-' + dateTime.slice(6, 8) + 'T' + dateTime.slice(8, 10) + ':' + dateTime.slice(10, 12) + ':' + dateTime.slice(12, 14));
   return date.toLocaleString();
 }
-
